266c6aa43e0f9265a34825e22296816380dafc8c
Anas Bellouzi <anbellouzi@gmail.com>
2020-11-14 15:04:17 -0800

    Implemented validation

--- routes/pets.js
+++ routes/pets.js
@@ -11,16 +11,17 @@ module.exports = (app) => {
     res.render('pets-new');
   });
 
-  // CREATE PET
+   // CREATE PET
   app.post('/pets', (req, res) => {
     var pet = new Pet(req.body);
 
     pet.save()
       .then((pet) => {
-        res.redirect(`/pets/${pet._id}`);
+        res.send({ pet: pet });
       })
       .catch((err) => {
-        // Handle Errors
+        // STATUS OF 400 FOR VALIDATIONS
+        res.status(400).send(err.errors);
       }) ;
   });
 
